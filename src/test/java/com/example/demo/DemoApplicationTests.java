054858bba168d60d70a4c267c7d892ff238b3a30
narutolw <2397401628@qq.com>
2022-02-11 22:42:12 +0800

    添加starter-logging依赖

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +10,13 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void test1() {
+		Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
+		logger.info("slf4j test");
+		System.out.println("你好");
 	}
 
 }
